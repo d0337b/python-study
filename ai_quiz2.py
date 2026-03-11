50979b3c19c1ad3fe255bc87e55b0c17a429373f
용현 <yonghyun@yonghyeon-ui-MacBookAir.local>
2026-03-11 23:21:26 +0900

    python3 basic quiz

---
+++ ai_quiz2.py
@@ -0,0 +1,8 @@
+numbers = [3,7,2,9,4,10,15,7,3,7]
+count = {}
+for n in numbers:
+    if n in count:
+        count[n] += 1
+    else:
+        count[n] = 1
+print(key=count.get)
---
+++ basic_py/dictionary_for.py
@@ -0,0 +1,7 @@
+student = {
+    "name": "yonghyun",
+    "age": 25,
+    "major": "Business"
+}
+for key in student:
+    print(key, student[key])
