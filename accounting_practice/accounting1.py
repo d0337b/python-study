f733dac27633c573552f2979e18d7bc7afa9398e
용현 <yonghyun@yonghyeon-ui-MacBookAir.local>
2026-03-12 18:00:23 +0900

    accounting practice 1

---
+++ accounting_practice/accounting1.py
@@ -0,0 +1,16 @@
+transactions = [
+    {"account":"cash", "debit":1000, "credit":0},
+    {"account":"sales", "debit":0, "credit":1000},
+    {"account":"cash", "debit":500, "credit":0},
+    {"account":"sales", "debit":0, "credit":500},
+    {"account":"expense", "debit":200, "credit":0}
+]
+result = {}
+for t in transactions:
+    account = t["account"]
+    debit = t["debit"]
+    if account in result:
+        result[account] += debit
+    else:
+        result[account] = debit
+print(result)
