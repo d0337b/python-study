8aa84a8d2b3c9c621e336ba025ede3f4541a6286
용현 <yonghyun@yonghyeon-ui-MacBookAir.local>
2026-03-12 15:05:46 +0900

    python3 basic practice

---
+++ function_study/count_numbers.py
@@ -0,0 +1,22 @@
+def counter_numbers(numbers):
+    count = {}
+    for n in numbers:
+        if n in count:
+            count[n] += 1
+        else:
+            count[n] = 1
+    return count
+
+def find_most_frequent(count):
+    max_number = None
+    max_count = 0
+
+    for key in count:
+        if count[key] > max_count:
+            max_count = count[key]
+            max_number = key
+    return max_number
+
+numbers = [3,7,2,9,4,10,15,7,3,7]
+result = find_most_frequent(counter_numbers(numbers))
+print("Most frequent number is :", result)
